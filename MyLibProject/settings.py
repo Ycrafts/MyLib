9288a9751978ff901e9aea2fbc7276ab30856b18
MrYonatan <yonatanassefa60@gmail.com>
2024-10-31 10:28:10 +0300

    switched to postgresql

--- MyLibProject/settings.py
+++ MyLibProject/settings.py
@@ -85,13 +85,16 @@ WSGI_APPLICATION = 'MyLibProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
+DATABASES={
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mylib',
+        'USER': 'postgres',
+        'PASSWORD': 'Password',
+        'HOST': 'localhost',
+        'PORT':'',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators

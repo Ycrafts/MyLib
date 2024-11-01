08d5c3cc5f5b1f0209a3396b9a3a608744b8047a
MrYonatan <yonatanassefa60@gmail.com>
2024-11-01 23:32:52 +0300

    before railway config for postgre

--- MyLibProject/settings.py
+++ MyLibProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s)4l)lqsa_&cz_=bo*wc%p@f6hhkl(98!k==u#_x+u&jdy521b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
